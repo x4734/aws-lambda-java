3f1df667e2a660628585e302b53cc4e8245873f0
x4734 <36784031+x4734@users.noreply.github.com>
2019-11-20 19:20:19 +0530

    AWS SNS  topic -lambda

---
+++ demo-aws-lambda-java/src/main/java/demo/lambda/fuction/handler/ReadSNSMsg.java
@@ -0,0 +1,18 @@
+package demo.lambda.fuction.handler;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class ReadSNSMsg implements RequestHandler<SNSEvent, Object> {
+	public Object handleRequest(SNSEvent request, Context context) {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+		context.getLogger().log("First messge Invocation started: " + timeStamp);
+		context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
+		timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+		context.getLogger().log("Invocation completed: " + timeStamp);
+		return null;
+	}
+}
